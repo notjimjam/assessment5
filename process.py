fb9cd8f5b9b9ab25d3774bc7f99c1c985adfef65
Jamie Smith <thejamiemsmith@gmail.com>
2022-03-11 09:57:02 -0700

    completed part 1

--- process.py
+++ process.py
@@ -1,12 +1,12 @@
-log_file = open("um-server-01.txt")
+log_file = open("um-server-01.txt") #this is opening the file allowing it to be used by the current file
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def sales_reports(log_file): #creating a function called sales_reports and passing in log_file as the parameter needed
+    for line in log_file: #this is a for loop iterating through each line in the file
+        line = line.rstrip() #this is stripping the white space off the right side of each line
+        day = line[0:3] #this is asking the loop to find the day located at the particular location on the line
+        if day == "Mon": #if statement to check if the day set on the previos line is equal to monday
+            print(line) #printing the completed line on our console
 
 
-sales_reports(log_file)
+sales_reports(log_file) #invoking the function, providing log_file as the parameter
